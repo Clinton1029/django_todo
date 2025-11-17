09aa9d8146d301197107664ac203b5b3386897ba
Clinton Yade <yadeclinton20@gmail.com>
2025-11-17 12:35:17 +0300

    added task class in the models.py

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=255)      # Task name
+    completed = models.BooleanField(default=False) # Done or not
+    created_at = models.DateTimeField(auto_now_add=True) # Auto timestamp
+
+    def __str__(self):
+        return self.title
