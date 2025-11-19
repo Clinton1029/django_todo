ed285adbc79e0b125f497e74953226072d2c0777
Clinton Yade <yadeclinton20@gmail.com>
2025-11-19 15:31:32 +0300

    added category to urls

--- todo/urls.py
+++ todo/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="home"),
-    path("delete/<int:id>/", views.delete_task, name="delete"),
+    path('', views.index, name='home'),
+    path('delete/<int:id>/', views.delete_task, name='delete'),
+    path('edit/<int:id>/', views.edit_task, name='edit'),
+    path('toggle/<int:id>/', views.toggle_complete, name='toggle'),
+    path('category/add/', views.add_category, name='add_category'),
 ]
