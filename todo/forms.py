88cc4643634cb449fde6167d9bb62479199bf54c
Clinton Yade <yadeclinton20@gmail.com>
2025-11-25 13:08:46 +0300

    updated forms.py

--- todo/forms.py
+++ todo/forms.py
@@ -1,7 +1,50 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from .models import Task, Category
 
 
+# ---------------------------------------------------------
+#  CUSTOM SIGNUP FORM (username, email, password1, password2)
+# ---------------------------------------------------------
+class CustomSignupForm(UserCreationForm):
+    email = forms.EmailField(
+        required=True,
+        widget=forms.EmailInput(attrs={
+            "placeholder": "Email address",
+            "class": "input"
+        })
+    )
+
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={
+            "placeholder": "Username",
+            "class": "input"
+        })
+    )
+
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(attrs={
+            "placeholder": "Create password",
+            "class": "input"
+        })
+    )
+
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(attrs={
+            "placeholder": "Confirm password",
+            "class": "input"
+        })
+    )
+
+    class Meta:
+        model = User
+        fields = ["username", "email", "password1", "password2"]
+
+
+# ---------------------------------------------------------
+#  TASK FORM
+# ---------------------------------------------------------
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
@@ -27,6 +70,9 @@ class TaskForm(forms.ModelForm):
         }
 
 
+# ---------------------------------------------------------
+#  CATEGORY FORM
+# ---------------------------------------------------------
 class CategoryForm(forms.ModelForm):
     class Meta:
         model = Category
